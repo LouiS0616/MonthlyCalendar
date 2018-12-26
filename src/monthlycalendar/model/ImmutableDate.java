2dc4572b0b1484139e86d86e4f3d6a4b9b8190f0
Loui Sakaki <e1352207@outlook.jp>
2018-12-26 16:11:19 +0900

    add constructor.

--- src/monthlycalendar/model/ImmutableDate.java
+++ src/monthlycalendar/model/ImmutableDate.java
@@ -13,6 +13,18 @@ public final class ImmutableDate {
 
         calendar_ = calendar;
     }
+    public ImmutableDate(int year, int month, int day) {
+        this(makeCalendar(year, month, day));
+    }
+
+    private static Calendar makeCalendar(int year, int month, int day) {
+        Calendar work = Calendar.getInstance();
+        work.set(Calendar.YEAR, year);
+        work.set(Calendar.MONTH, month-1);
+        work.set(Calendar.DATE, day);
+
+        return work;
+    }
 
     public ImmutableDate prev() {
         Calendar work = (Calendar)calendar_.clone();
