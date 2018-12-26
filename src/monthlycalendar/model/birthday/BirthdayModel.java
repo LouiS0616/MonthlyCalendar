186957b19f2e9c7880eb4b680e5ab71ad2c50b5c
Loui Sakaki <e1352207@outlook.jp>
2018-12-26 18:34:36 +0900

    debug; to read all line of birthday.csv.

--- src/monthlycalendar/model/birthday/BirthdayModel.java
+++ src/monthlycalendar/model/birthday/BirthdayModel.java
@@ -1,10 +1,12 @@
 package monthlycalendar.model.birthday;
 
+import monthlycalendar.model.ImmutableDate;
 import monthlycalendar.model.ImmutableDateWithoutYear;
 import monthlycalendar.utility.PropertyWrapper;
 import monthlycalendar.utility.csv.CSVReader;
 import monthlycalendar.utility.csv.RecordParser;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,18 +25,22 @@ public class BirthdayModel {
             property_.getProperty("birthdayFilePath", "birthday.csv")
         );
         try(CSVReader<Birthday> reader = new CSVReader<>(path, new BirthdayParser(), true)) {
-            Birthday birthday = reader.readRecord();
-            ImmutableDateWithoutYear date = birthday.date.withoutYear();
+            Birthday birthday;
+            while((birthday = reader.readRecord()) != null) {
+                ImmutableDateWithoutYear date = birthday.date.withoutYear();
 
-            if(birthdayMap_.containsKey(date)) {
-                birthdayMap_.get(date).add(birthday);
-            }
-            else {
-                List<Birthday> list = new ArrayList<>();
-                list.add(birthday);
+                if(birthdayMap_.containsKey(date)) {
+                    birthdayMap_.get(date).add(birthday);
+                } else {
+                    List<Birthday> list = new ArrayList<>();
+                    list.add(birthday);
 
-                birthdayMap_.put(date, list);
+                    birthdayMap_.put(date, list);
+                }
             }
+        }
+        catch(EOFException e) {
+            // pass
         }
         catch(IOException e) {
             System.err.println(String.format("Failed to read '%s'.", path));
@@ -42,5 +48,16 @@ public class BirthdayModel {
         catch(RecordParser.InvalidRecordException e) {
             System.err.println(e.getMessage());
         }
+
+        for(List<Birthday> list: birthdayMap_.values()) {
+            for(Birthday birthday: list) {
+                System.out.println(birthday);
+            }
+        }
+    }
+
+    //
+    public static boolean isSomeonesBirthday(ImmutableDate date) {
+        return birthdayMap_.containsKey(date.withoutYear());
     }
 }
